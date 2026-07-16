3312305d22efebf44a3aa0a4c9db601910a980cd
khalid altaj <khalid.altaj.94@gmail.com>
2026-07-16 17:07:57 +0400

    تعديل الروابط لاستخدام HashRouter وتصحيح مسارات النشر

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sudapost-app/',
+  // تأكد أن المسار يبدأ وينتهي بـ "/" ويتضمن اسم المستودع الخاص بك
+  base: '/sudapost-app/', 
 })
